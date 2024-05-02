946e85268fdf95dd8a715ab5edaa8a3b4ffd0c29
suruzzaman <sajukaligong21@gmail.com>
2024-05-02 12:58:05 +0600

    shop page start

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,7 +1,28 @@
+import { NavLink } from 'react-router-dom';
+import './Shop.css';
 const Shop = () => {
   return (
     <div>
-      <h1>This is Shop Page</h1>
+      <div>
+        {/* banner */}
+        <div className="s-banner w-full h-[40vh] md:h-[50vh]">
+          <div className="px-3 flex flex-col justify-center h-full md:px-10 lg:px-16">
+            <h1 className=" text-4xl md:text-7xl text-[#EEEEEE] s-title">
+              JACKETS & COATS
+            </h1>
+            <ul className=" flex flex-wrap text-[vw] mt-3  gap-x-4">
+              <li>ALL</li>
+              <li>COMPANY</li>
+              <li>
+                <NavLink>FASHION</NavLink>
+              </li>
+              <li>STYLE</li>
+              <li>TRENDS</li>
+              <li>BEAUTY</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
