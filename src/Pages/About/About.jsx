fbb0cd4356515451220c6fb8453e71b156f6d6b7
suruzzaman <sajukaligong21@gmail.com>
2024-05-02 11:04:07 +0600

    about page and contact page done

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,85 @@
+import { TbLockOpen } from 'react-icons/tb';
+import about1 from '../../assets/img/about-1.jpg';
+import aboutBanner from '../../assets/img/about-2 (1).jpg';
+import { FaHeadset } from 'react-icons/fa6';
+import { LuShieldCheck } from 'react-icons/lu';
 const About = () => {
   return (
     <div>
-      <h1>This is About Page</h1>
+      <h1 className="px-5 md:px-22 lg:px-60 text-3xl font-medium mb-9">
+        ABOUT UOMO
+      </h1>
+      <div className="mb-4 w-full">
+        <img className="w-full" src={aboutBanner} alt="" />
+      </div>
+      <div className="px-5 md:px-22 lg:px-60 ">
+        <p className="text-xl font-medium">OUR STORY</p>
+        <p className="my-4 leading-7">
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+        <p className="text-sm my-4 leading-6 text-gray-700">
+          Saw wherein fruitful good days image them, midst, waters upon, saw.
+          Seas lights seasons. Fourth hath rule Evening Creepeth own lesser
+          years itself so seed fifth for grass evening fourth shall you are unto
+          that. Had. Female replenish for yielding so saw all one to yielding
+          grass you all air sea it, open waters subdue, hath. Brought second
+          Made. Be. Under male male, firmament, beast had light after fifth
+          forth darkness thing hath sixth rule night multiply him life give they
+          are great.
+        </p>
+        <div className="flex flex-col md:flex-row gap-y-4 justify-between items-center gap-x-20 mt-10">
+          <div>
+            <p>Our Mission</p>
+            <p>
+              Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </p>
+          </div>
+          <div>
+            <p>Our Vision</p>
+            <p>
+              Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row justify-between gap-y-2 items-center gap-x-4 mt-10">
+          <div className="md:w-1/2">
+            <img className="w-full" src={about1} alt="" />
+          </div>
+          <div className="md:w-1/2">
+            <p className="text-xl font-medium mb-3">The Company</p>
+            <p className="leading-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet
+              sapien dignissim a elementum. Sociis metus, hendrerit mauris id
+              in. Quis sit sit ultrices tincidunt euismod luctus diam. Turpis
+              sodales orci etiam phasellus lacus id leo. Amet turpis nunc, nulla
+              massa est viverra interdum. Praesent auctor nulla morbi non
+              posuere mattis. Arcu eu id maecenas cras.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row gap-y-8 justify-between items-center my-16">
+          <div className="flex flex-col justify-center items-center">
+            <TbLockOpen className="text-7xl"></TbLockOpen>
+            <p className="font-medium py-2">FAST AND FREE DELIVERY</p>
+            <p>Free delivery for all orders over $140</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <FaHeadset className="text-7xl"></FaHeadset>
+            <p className="font-medium py-2">24/7 CUSTOMER SUPPORT</p>
+            <p>Friendly 24/7 customer support</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <LuShieldCheck className="text-7xl"></LuShieldCheck>
+            <p className="font-medium py-2">MONEY BACK GUARANTEE</p>
+            <p>We return money within 30 days</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,64 @@
 const Contact = () => {
   return (
-    <div>
-      <h1>This is Contact Page</h1>
+    <div className="my-16">
+      <h1 className="px-5 md:px-22 lg:px-60 text-3xl font-medium mb-9">
+        CONTACT US
+      </h1>
+      <div className="px-5 md:px-22 lg:px-60">
+        <div className="flex flex-col gap-y-6 md:flex-row justify-between items-center md:gap-x-10">
+          <div>
+            <p className="text-2xl font-medium mb-4">Store in London</p>
+            <p className="my-3">
+              1418 River Drive, Suite 35 Cottonhall, CA 9622 United Kingdom
+            </p>
+            <p>
+              sale@uomo.com <br /> +44 20 7123 4567
+            </p>
+          </div>
+          <div>
+            <p className="text-2xl font-medium mb-4">Store in Istanbul</p>
+            <p className="my-3">
+              1418 River Drive, Suite 35 Cottonhall, CA 9622 United Kingdom
+            </p>
+            <p>
+              sale@uomo.com <br /> +44 20 7123 4567
+            </p>
+          </div>
+        </div>
+        <form className="mt-20">
+          <h1 className="text-2xl font-medium">Get In Touch</h1>
+          <div className="mt-10">
+            <input
+              type="text"
+              name=""
+              className="w-full text-xl py-3 px-3 border-2 focus:outline-none border-gray-300 placeholder:italic"
+              placeholder="Name *"
+              id=""
+            />
+          </div>
+          <div className="mt-5 md:mt-10">
+            <input
+              type="email"
+              name=""
+              className="w-full text-xl py-3 px-3 border-2 focus:outline-none border-gray-300 placeholder:italic"
+              placeholder="Email address *"
+              id=""
+            />
+          </div>
+          <div className="mt-5 md:mt-10">
+            <textarea
+              name=""
+              id=""
+              className="w-full border-2 border-gray-300 focus:outline-none py-3 px-3 placeholder:italic text-xl"
+              placeholder="Your Message"
+              rows="8"
+            ></textarea>
+          </div>
+          <button className="text-xl md:mt-4 font-medium bg-slate-700 hover:bg-slate-800 duration-200 cursor-pointer text-white px-8 py-3">
+            SUBMIT
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
